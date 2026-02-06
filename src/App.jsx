c7a61eda20e5da251df6268a6c1e1f7147e00c88
ReignCC <reignrcultivo1920@gmail.com>
2026-02-06 12:19:58 +0800

    scroll commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
+import ScrollToTop from './components/ScrollToTop'
 import HomePage from './pages/HomePage'
 import PublicationPage from './pages/PublicationPage'
 import JournalPage from './pages/JournalPage'
@@ -9,6 +10,7 @@ import ContactPage from './pages/ContactPage'
 function App() {
   return (
     <Layout>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/publication" element={<PublicationPage />} />
